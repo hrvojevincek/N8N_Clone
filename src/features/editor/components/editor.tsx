b042624fcd2e3a49219bbc1183b8f7ad56ea28db
hrvoje vincek <vincekhrvoje@gmail.com>
2025-11-02 15:44:17 +0100

    add workflow page

---
+++ src/features/editor/components/editor.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ErrorView, LoadingView } from "@/components/entity-components";
+import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+
+export const EditorLoading = () => {
+  return <LoadingView message="Loading editor..." />;
+};
+
+export const EditorError = () => {
+  return <ErrorView message="Error loading editor..." />;
+};
+
+export const Editor = ({ workflowId }: { workflowId: string }) => {
+  console.log("workflowId", workflowId);
+  const { data: workflow } = useSuspenseWorkflow(workflowId);
+
+  return <div>{JSON.stringify(workflow, null, 2)}</div>;
+};
