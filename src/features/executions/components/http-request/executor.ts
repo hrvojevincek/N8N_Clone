5b7e3cc461702dd5d78d254fa8cabb53939f3d14
hrvoje vincek <vincekhrvoje@gmail.com>
2025-11-08 16:27:12 +0100

    node templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,11 +1,18 @@
 import { NodeExecutor } from "../../types";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
+import Handlebars from "handlebars";
+
+Handlebars.registerHelper("json", (context) => {
+  const jsonString = JSON.stringify(context, null, 2);
+  const safeString = new Handlebars.SafeString(jsonString);
+  return safeString;
+});
 
 type HttpRequestData = {
-  variableName?: string;
-  method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
-  endpoint?: string;
+  variableName: string;
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  endpoint: string;
   body?: string;
 };
 
@@ -24,15 +31,21 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       "HTTP Request node: No variable name configured"
     );
   }
+  if (!data.method) {
+    throw new NonRetriableError("HTTP Request node: No method configured");
+  }
 
   const result = await step.run("http-request", async () => {
-    const endpoint = data.endpoint!;
-    const method = data.method || "GET";
+    const endpoint = Handlebars.compile(data.endpoint)(context);
+    const method = data.method;
 
     const options: KyOptions = { method };
 
     if (["POST", "PUT", "PATCH"].includes(method)) {
-      options.body = data.body;
+      const resolved = Handlebars.compile(data.body || {})(context);
+      JSON.parse(resolved);
+
+      options.body = resolved;
       options.headers = {
         "Content-Type": "application/json",
       };
@@ -52,16 +65,9 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       },
     };
 
-    if (data.variableName) {
-      return {
-        ...context,
-        [data.variableName]: responsePayload,
-      };
-    }
-
     return {
       ...context,
-      ...responsePayload,
+      [data.variableName]: responsePayload,
     };
   });
 
